ae0e26730284490b8cf57fa2ad418c3f130048cf
Ester Ribeiro <116028454+esterrds@users.noreply.github.com>
2022-11-30 08:30:31 -0300

    Update car.dart

--- lib/repo/models/car.dart
+++ lib/repo/models/car.dart
@@ -1,15 +1,10 @@
 //lista de carros e contador
 class Car {
-  Car.fromJson(
-    this.numeroDoCarro,
-    this.nomeDaEquipe,
-  );
-
   int _numeroDeVoltas = 0;
   final int numeroDoCarro;
   final String nomeDaEquipe;
 
-  Car(this._numeroDeVoltas, this.nomeDaEquipe, this.numeroDoCarro);
+  Car(this.nomeDaEquipe, this.numeroDoCarro);
 
   int getVoltas() => _numeroDeVoltas;
 
@@ -19,8 +14,8 @@ class Car {
     } else {
       _numeroDeVoltas++;
     }
-    this._numeroDeVoltas = _numeroDeVoltas;
-    print("carro: ${this.numeroDoCarro}, voltas: ${this._numeroDeVoltas}");
+    _numeroDeVoltas = _numeroDeVoltas;
+    print("carro: $numeroDoCarro, voltas: $_numeroDeVoltas");
   }
 
   void decrement() {
@@ -29,21 +24,29 @@ class Car {
     } else {
       _numeroDeVoltas--;
     }
-    this._numeroDeVoltas = _numeroDeVoltas;
-    print("carro: ${this.numeroDoCarro}, voltas: ${this._numeroDeVoltas}");
+    _numeroDeVoltas = _numeroDeVoltas;
+    print("carro: $numeroDoCarro, voltas: $_numeroDeVoltas");
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'equipe': nomeDaEquipe,
-      'numero': numeroDoCarro.toString(),
-      'voltas': _numeroDeVoltas.toString()
+  void teste2() {
+    var cadastro = {
+      1: nomeDaEquipe,
+      2: numeroDoCarro.toString(),
+      3: _numeroDeVoltas.toString()
     };
+
+    myNewList() {
+      return cadastro.entries.map((e) {
+        return e.value;
+      }).toList();
+    }
+
+    print(myNewList());
   }
 
   @override
   String toString() {
     // TODO: implement toString
-    return "Equipes: {carro: ${this.numeroDoCarro}, equipe: ${this.nomeDaEquipe}, voltas: ${this._numeroDeVoltas}}";
+    return "Equipes: {carro: ${numeroDoCarro}, equipe: ${nomeDaEquipe}, voltas: ${_numeroDeVoltas}}";
   }
 }
