ec12c6f6babd2fc9653cda0edcd72fef77218f63
Ester Ribeiro <116028454+esterrds@users.noreply.github.com>
2022-12-27 17:40:06 -0300

    Update main_list.dart

--- lib/presentation/main_list.dart
+++ lib/presentation/main_list.dart
@@ -1,3 +1,7 @@
+import 'package:enduro_app/bloc/Connectivity/connectivity_cubit.dart';
+import 'package:enduro_app/presentation/colors.dart';
+import 'package:enduro_app/presentation/nav_bar.dart';
+import 'package:enduro_app/repo/models/car.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,75 +9,108 @@ import '../bloc/ContadorCubit/contador_cubit.dart';
 
 //página do contador
 
-class MainList extends StatelessWidget {
+class MainList extends StatefulWidget {
   const MainList({super.key});
 
   @override
+  State<MainList> createState() => _MainListState();
+}
+
+class _MainListState extends State<MainList> {
+  @override
   Widget build(BuildContext context) {
     ContadorCubit cubit = BlocProvider.of<ContadorCubit>(context);
+    ConnectivityCubit conCubit = BlocProvider.of<ConnectivityCubit>(context);
+    List<Car> selecionados = [];
 
     return BlocBuilder<ContadorCubit, ContadorState>(
       builder: (context, state) {
         return Scaffold(
           //organização dos itens na lista
-          //padding: const EdgeInsets.symmetric(horizontal: 13.0),
           body: ListView.separated(
             itemBuilder: (BuildContext context, int index) {
               return Builder(builder: (context) {
-                return Container(
-                  alignment: Alignment.center,
-                  width: 100,
-                  height: 100,
-                  child: Dismissible(
-                    //excluir itens da lista
-                    background: Container(
-                      color: Colors.red,
-                    ),
-                    key: ValueKey<dynamic>(cubit.carList[index]),
-                    onDismissed: (DismissDirection direction) {
-                      cubit.carList.removeAt(index);
-                      final snackBar = const SnackBar(
-                          content: Text("Carro removido.")
-                          //action: SnackBarAction(
-                          /*label: 'Desfazer',
+                return Dismissible(
+                  //excluir itens da lista
+                  background: Container(
+                    color: Colors.red,
+                  ),
+                  key: ValueKey<dynamic>(cubit.carList[index]),
+                  onDismissed: (DismissDirection direction) {
+                    cubit.carList.removeAt(index);
+                    const snackBar = SnackBar(content: Text("Carro removido.")
+                        //action: SnackBarAction(
+                        /*label: 'Desfazer',
                               onPressed: (() {
                                 //retorna com o carro excluído (falta fazer)
                               }),
                                 )*/
-                          );
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(cubit.carList[index].numeroDoCarro.toString(),
-                            style: const TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold)),
-                        Text(cubit.carList[index].nomeDaEquipe,
-                            style: const TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.w500)),
-                        Text(
-                          cubit.carList[index].getVoltas().toString(),
+                        );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: ListTile(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                    title: Text(cubit.carList[index].nomeDaEquipe),
+                    subtitle: Text(
+                        "Carro: ${cubit.carList[index].numeroDoCarro.toString()}, voltas: ${cubit.carList[index].getVoltas().toString()}"),
+                    trailing: Icon(Icons.arrow_drop_down_sharp),
+                    onTap: (() {
+                      cubit.carList[index].increment();
+                      cubit.rebuild();
+                    }),
+                    selected: selecionados.contains(cubit.carList[index]),
+                    selectedColor: deepestBlue,
+                    onLongPress: (() {
+                      print("pressionado");
+                      setState(() {
+                        (selecionados.contains(cubit.carList[index]))
+                            ? selecionados.remove(cubit.carList[index])
+                            : selecionados.add(cubit.carList[index]);
+
+                        if (state is ConnectivityDisconnected) {
+                          alertFailed(context);
+                        } else if (state is ConnectivityConnected) {
+                          conCubit.publishTest(cubit);
+                          alertSucess(context);
+                        }
+                      });
+                      alertSucess(context);
+                      print(cubit.carList[index].nomeDaEquipe);
+                    }),
+
+                    //child: Row
+                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    /*children: <Widget>[
+                      Text(cubit.carList[index].numeroDoCarro.toString(),
                           style: const TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        //botão de incremento
-                        GestureDetector(
-                          child: const Icon(Icons.arrow_upward),
-                          onTap: () {
-                            cubit.carList[index].increment();
-                            cubit.rebuild();
-                          },
-                        ),
-                        //botão de decremento
-                        GestureDetector(
-                          child: const Icon(Icons.arrow_downward),
-                          onTap: () {
-                            cubit.carList[index].decrement();
-                            cubit.rebuild();
-                          },
-                        ),
-                        GestureDetector(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                      Text(cubit.carList[index].nomeDaEquipe,
+                          style: const TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w500)),
+                      Text(
+                        cubit.carList[index].getVoltas().toString(),
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      //botão de incremento
+                      GestureDetector(
+                        child: const Icon(Icons.arrow_upward),
+                        onTap: () {
+                          cubit.carList[index].increment();
+                          cubit.rebuild();
+                        },
+                      ),
+                      //botão de decremento
+                      GestureDetector(
+                        child: const Icon(Icons.arrow_downward),
+                        onTap: () {
+                          cubit.carList[index].decrement();
+                          cubit.rebuild();
+                        },
+                      ),*/
+
+                    /*GestureDetector(
                           child: const Icon(Icons.check),
                           onLongPress: () {
                             print("Long press");
@@ -99,9 +136,8 @@ class MainList extends StatelessWidget {
                               ),
                             );
                           },
-                        ),
-                      ],
-                    ),
+                        ),*/
+                    //],
                   ),
                 );
               });
